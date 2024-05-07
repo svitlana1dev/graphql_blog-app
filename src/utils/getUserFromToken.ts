aeee8ef53084c6f00632dfda6e151902d27df5bb
svitlana1dev <ponomarchuk.svitlana.dev@gmail.com>
2024-05-07 20:13:48 +0300

    added new env

--- src/utils/getUserFromToken.ts
+++ src/utils/getUserFromToken.ts
@@ -1,9 +1,9 @@
 import JWT from "jsonwebtoken";
-import { JSON_SIGNATURE } from "../keys";
+require("dotenv").config();
 
 export const getUserFromTheToken = (token: string) => {
   try {
-    return JWT.verify(token, JSON_SIGNATURE) as {
+    return JWT.verify(token, `${process.env.JSON_SIGNATURE}`) as {
       userId: number;
     };
   } catch (err) {
